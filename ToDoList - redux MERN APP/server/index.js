c7e6c419f190ddb0a1650ea15c6544840f85d586
Shubham Jain <sj18071998@gmail.com>
2023-02-26 23:43:08 +0530

    redux app

--- ToDoList - redux MERN APP/server/index.js
+++ ToDoList - redux MERN APP/server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import Routes from './routes/route.js';
+import cors from 'cors';
 import './database/db.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended:true}));
 
--- ToDoList - redux MERN APP/server/routes/route.js
+++ ToDoList - redux MERN APP/server/routes/route.js
@@ -1,8 +1,16 @@
-import express from "express";
-import { addToDo } from "../controllers/todo-controller.js";
+import express, { Router } from 'express';
+
+
+import { addTodo, getAllTodos, toggleTodoDone, updateTodo, deleteTodo } from '../controller/todo-controller.js';
 
 const route = express.Router();
 
-route.post('/todos', addToDo);
+
+route.post('/todos', addTodo)
+route.get('/todos', getAllTodos);
+route.get('/todos/:id', toggleTodoDone);
+route.put('/todos/:id', updateTodo);
+route.delete('/todos/:id', deleteTodo);
+
 
 export default route;
